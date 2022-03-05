66c6899a51bc2056344776b091518e222e307bfd
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2022-03-05 10:13:07 -0300

    loading info from data file

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ function App() {
     <div className="grid-container">
       <header className="row">
         <div>
-          <a href="index.html" className="brand">
+          <a href="/" className="brand">
             Amazona
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div className="row center">
           {data.products.map((product) => (
-            <div className="card">
-              <a href="product.html">
-                <img className="medium" src="./images/p1.jpg" alt="product" />
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
               </a>
               <div className="card-body">
-                <a href="product.html">
-                  <h2>Nike Slim shirts</h2>
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
                 </a>
                 <div className="rating">
                   <span>
@@ -41,7 +45,7 @@ function App() {
                     <i className="fa fa-star"></i>
                   </span>
                 </div>
-                <div className="price">$120</div>
+                <div className="price">{product.price}</div>
               </div>
             </div>
           ))}
