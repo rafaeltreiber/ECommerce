32a77cbb3c3adce3b729f7bbc9878e004169409f
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2022-03-04 22:33:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import data from "./data";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="index.html" className="brand">
+            Amazona
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <div className="card">
+              <a href="product.html">
+                <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim shirts</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">$120</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All rights reserved</footer>
     </div>
   );
 }
