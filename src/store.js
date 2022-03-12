b54b2f99e3fb9eee474a783ebb3667e3684f4396
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2022-03-12 19:13:46 -0300

    added redux to productscreen

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
-import data from "./data";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
